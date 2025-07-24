d0f673a987538427583fd2417e7a7823fa56b299
Youssef <youssefislam2682003@gmail.com>
2025-07-24 19:45:56 +0200

    Update Main_class.py

--- Main_class.py
+++ Main_class.py
@@ -40,7 +40,7 @@ class FileHandler:
 
     def __str__(self):
         return f"FileHandler({self._file_path})"
-    @deco("green")
+    @deco("red")
    
     def read_lines(self):
 
@@ -106,7 +106,8 @@ def main():
 
         print("\n📄 Reading file1.txt:")
         for line in handler1.read_lines():
-         print(" ",colored_text(line, "green"))
+            pass
+         #print(" ",colored_text(line, "green"))
 
         print("\n🔗 Concatenating both files into multi_output.txt...")
         result_handler = handler1.concat_multiple(handler2)
